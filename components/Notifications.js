c608bb373cef9ab369c728b8c3082794762d2df2
Wali  Ullah <waliullah0540@gmail.com>
2023-01-01 00:12:34 +0500

    resp fixes

--- components/Notifications.js
+++ components/Notifications.js
@@ -6,8 +6,8 @@ import HeaderNotificationsIcon from './SVGs/HeaderIcons/HeaderNotificationsIcon'
 
 const Index = () => {
     return (
-        <div className="max-w-sm px-4 ">
-            <Popover className="relative">
+        <div className="max-w-sm px-4">
+            <Popover className="relative ">
                 {({ open }) => (
                     <>
                         <Popover.Button
@@ -28,7 +28,7 @@ const Index = () => {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute -right-28 md:right-0 z-50 pt-4 ">
+                            <Popover.Panel className="absolute -right-28 md:right-0 z-[99] pt-4 ">
                                 <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 h-[512px] bg-white py-[18px] px-[18px] notification_container">
                                     <div className='flex flex-row items-center justify-between'>
                                         <h1 className='text-[#1E1E1E] font-bold text-base'>Notification</h1>
@@ -44,10 +44,10 @@ const Index = () => {
                                                     className="mb-6 flex justify-between items-start"
                                                 >
                                                     <div className='flex gap-x-3'>
-                                                        <div className='h-[38px] w-[38px] bg-black flex items-center justify-center rounded-lg'>
+                                                        <div className='h-[38px] w-[38px] bg-black flex items-center justify-center rounded-lg relative'>
                                                             <img src='/lock.svg' alt='' />
                                                         </div>
-                                                        <div>
+                                                        <div className='flex-1'>
                                                             <h6 className='text-[15px] font-semibold text-black'>Password changed</h6>
                                                             <p className='text-xs text-black/60'>Password has changed successfully</p>
                                                         </div>

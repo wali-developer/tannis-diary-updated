e496d3b4b352b5bb427fe421043049509bc59a6d
unknown <saliheenafridi0@gmail.com>
2023-01-08 22:09:50 +0500

    salih

--- components/Dashboard/TotalMembers.js
+++ components/Dashboard/TotalMembers.js
@@ -1,66 +1,69 @@
-import React from 'react';
+import React from "react";
 
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const data = [
-    { name: 'Group A', value: 100, color: '#3B82F6', name: 'Beginer' },
-    { name: 'Group B', value: 100, color: '#50119C', name: 'Competitive' },
-    { name: 'Group C', value: 100, color: '#FFA600', name: 'Advanced' },
-    { name: 'Group D', value: 100, color: '#B3DD07', name: 'Intermediate' },
-    { name: 'Group E', value: 100, color: '#000000', name: 'Coach' },
+  { name: "Group A", value: 100, color: "#3B82F6", name: "Beginer" },
+  { name: "Group B", value: 100, color: "#50119C", name: "Competitive" },
+  { name: "Group C", value: 100, color: "#FFA600", name: "Advanced" },
+  { name: "Group D", value: 100, color: "#B3DD07", name: "Intermediate" },
+  { name: "Group E", value: 100, color: "#000000", name: "Coach" },
 ];
-const COLORS = ['#3B82F6', '#50119C', '#FFA600', '#B3DD07', '#000000'];
+const COLORS = ["#3B82F6", "#50119C", "#FFA600", "#B3DD07", "#000000"];
 
+const TotalMembers = () => {
+  return (
+    <div className="sm:flex-1 h-[420px] sm:h-[318px] bg-white  rounded-[10px] pt-[19px] pb-[15px] px-[13px] min-w-[300px] ">
+      {/* Heading start */}
+      <div>
+        <h1 className="text-[#A0A0A3] font-normal text-sm">Total members</h1>
+        <h1 className="text-[#1E1E1E] font-bold text-[24px]">21,500</h1>
+      </div>
+      {/* Heading end */}
 
-const TotalMembers = () =>
-{
-    return (
-        <div className='sm:flex-1 h-[318px] bg-white  rounded-[10px] pt-[19px] pb-[15px] px-[13px] min-w-[300px] '>
-            {/* Heading start */ }
-            <div>
-                <h1 className='text-[#A0A0A3] font-normal text-sm'>Total members</h1>
-                <h1 className='text-[#1E1E1E] font-bold text-[24px]'>21,500</h1>
+      <ResponsiveContainer width="80%" height="90%">
+        <PieChart width={500} height={200}>
+          <Pie
+            data={data}
+            cx={150}
+            cy={80}
+            innerRadius={50}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={0}
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <div className="flex gap-3 flex-wrap -mt-32 sm:-mt-20">
+        {data?.map((item, index) => {
+          return (
+            <div
+              className="flex flex-row  items-center space-x-[6px]"
+              key={index + ""}
+            >
+              <div
+                className={`w-[18px] h-[12px]  rounded-sm `}
+                style={{
+                  backgroundColor: item?.color,
+                }}
+              ></div>
+              <h1 className="text-gray-700 mb-[0.4px] font-medium text-sm">
+                <span className="font-semibold">{"20% "}</span> {item?.name}
+              </h1>
             </div>
-            {/* Heading end */ }
-            <ResponsiveContainer width="80%" height="90%">
-                <PieChart width={ 500 } height={ 200 }>
-                    <Pie
-                        data={ data }
-                        cx={ 150 }
-                        cy={ 80 }
-                        innerRadius={ 50 }
-                        outerRadius={ 80 }
-                        fill="#8884d8"
-                        paddingAngle={ 0 }
-                        dataKey="value"
-
-                    >
-                        { data.map((entry, index) => (
-                            <Cell key={ `cell-${index}` }
-                                fill={ COLORS[index % COLORS.length] } />
-                        )) }
-                    </Pie>
-                </PieChart>
-            </ResponsiveContainer>
-            <div className='flex gap-3 flex-wrap -mt-20'>
-                { data?.map((item, index) =>
-                {
-                    return (
-                        <div
-                            className='flex flex-row  items-center space-x-[6px]'
-                            key={ index + '' }>
-                            <div className={ `w-[18px] h-[12px]  rounded-sm ` }
-                                style={ {
-                                    backgroundColor: item?.color
-                                } }>
-                            </div>
-                            <h1 className='text-gray-700 mb-[0.4px] font-medium text-sm'><span className='font-semibold'>{ '20% ' }</span> { item?.name }</h1>
-                        </div>
-                    )
-                }) }
-            </div>
-        </div>
-    );
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default TotalMembers;
--- components/Layout/MobileSidebar.js
+++ components/Layout/MobileSidebar.js
@@ -1,154 +1,164 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import CouchesIcon from '../SVGs/SidebarIcons/CouchesIcon';
-import DashboardIcon from '../SVGs/SidebarIcons/DashboardIcon';
-import EventsIcon from '../SVGs/SidebarIcons/EventsIcon';
-import MathcesIcon from '../SVGs/SidebarIcons/MathcesIcon';
-import PlayersIcon from '../SVGs/SidebarIcons/PlayersIcon';
-import Tournaments from '../SVGs/SidebarIcons/Tournaments';
-import TrainingIcon from '../SVGs/SidebarIcons/TrainingIcon';
+import { useRouter } from "next/router";
+import React from "react";
+import CouchesIcon from "../SVGs/SidebarIcons/CouchesIcon";
+import DashboardIcon from "../SVGs/SidebarIcons/DashboardIcon";
+import EventsIcon from "../SVGs/SidebarIcons/EventsIcon";
+import MathcesIcon from "../SVGs/SidebarIcons/MathcesIcon";
+import PlayersIcon from "../SVGs/SidebarIcons/PlayersIcon";
+import Tournaments from "../SVGs/SidebarIcons/Tournaments";
+import TrainingIcon from "../SVGs/SidebarIcons/TrainingIcon";
 
 const MobileSidebar = ({ visibility, onCloseButtonClicked }) => {
-    const router = useRouter()
+  const router = useRouter();
 
-    // const sidebardata = [
-    //     {
-    //         title: 'Dashboard',
-    //         Icon: <DashboardIcon />,
-    //         path: '/dashboard'
-    //     },
-    //     {
-    //         title: 'Training',
-    //         Icon: <TrainingIcon />,
-    //         path: '/training'
-    //     },
-    //     {
-    //         title: 'Matches',
-    //         Icon: <Tournaments />,
-    //         path: '/matches'
-    //     },
-    //     {
-    //         title: 'Tournaments',
-    //         Icon: <Tournaments />,
-    //         path: '/tournaments'
-    //     },
-    //     {
-    //         title: 'Players',
-    //         Icon: <PlayersIcon />,
-    //         path: '/players'
-    //     },
-    //     {
-    //         title: 'Coaches',
-    //         Icon: <CouchesIcon />,
-    //         path: '/coaches'
-    //     },
-    //     {
-    //         title: 'Events',
-    //         Icon: <EventsIcon />,
-    //         path: '/events'
-    //     }
-    // ];
+  // const sidebardata = [
+  //     {
+  //         title: 'Dashboard',
+  //         Icon: <DashboardIcon />,
+  //         path: '/dashboard'
+  //     },
+  //     {
+  //         title: 'Training',
+  //         Icon: <TrainingIcon />,
+  //         path: '/training'
+  //     },
+  //     {
+  //         title: 'Matches',
+  //         Icon: <Tournaments />,
+  //         path: '/matches'
+  //     },
+  //     {
+  //         title: 'Tournaments',
+  //         Icon: <Tournaments />,
+  //         path: '/tournaments'
+  //     },
+  //     {
+  //         title: 'Players',
+  //         Icon: <PlayersIcon />,
+  //         path: '/players'
+  //     },
+  //     {
+  //         title: 'Coaches',
+  //         Icon: <CouchesIcon />,
+  //         path: '/coaches'
+  //     },
+  //     {
+  //         title: 'Events',
+  //         Icon: <EventsIcon />,
+  //         path: '/events'
+  //     }
+  // ];
 
-    const sidebardata = [
-        {
-            title: 'Dashboard',
-            Icon: <DashboardIcon />,
-            path: '/dashboard'
-        },
-        {
-            title: 'Training',
-            Icon: <CouchesIcon />,
-            path: '/training'
-        },
-        {
-            title: 'Matches',
-            Icon: <Tournaments />,
-            path: '/matches'
-        },
-        {
-            title: 'Tournaments',
-            Icon: <Tournaments />,
-            path: '/tournaments'
-        },
-        {
-            title: 'Players',
-            Icon: <PlayersIcon />,
-            path: '/players'
-        },
-        {
-            title: 'Coaches',
-            Icon: <CouchesIcon />,
-            path: '/coaches'
-        },
-        {
-            title: 'Events',
-            Icon: <EventsIcon />,
-            path: '/events'
-        }
-    ];
+  const sidebardata = [
+    {
+      title: "Dashboard",
+      Icon: <DashboardIcon />,
+      path: "/dashboard",
+    },
+    {
+      title: "Training",
+      Icon: <CouchesIcon />,
+      path: "/training",
+    },
+    {
+      title: "Matches",
+      Icon: <Tournaments />,
+      path: "/matches",
+    },
+    {
+      title: "Tournaments",
+      Icon: <Tournaments />,
+      path: "/tournaments",
+    },
+    {
+      title: "Players",
+      Icon: <PlayersIcon />,
+      path: "/players",
+    },
+    {
+      title: "Coaches",
+      Icon: <CouchesIcon />,
+      path: "/coaches",
+    },
+    {
+      title: "Events",
+      Icon: <EventsIcon />,
+      path: "/events",
+    },
+  ];
 
-    return (
-        <div className={`fixed z-50 inset-0  lg:hidden bg-black/5 transition-all duration-1000 ${visibility ? " -translate-x-[0px]" : " -translate-x-[1024px]"}`}>
-            <div className='w-full h-full flex flex-row'>
-                <div className='self-stretch flex-1 bg-white pt-5 px-4 flex flex-col'>
-                    <div className={`self-end transition-all duration-1000 cursor-pointer ${visibility ? "opacity-100" : "opacity-0"} `}
-                        onClick={onCloseButtonClicked}
-                    >
-                        <CLoseIcon />
-                    </div>
+  return (
+    <div
+      className={`fixed z-50 inset-0  lg:hidden bg-black/5 transition-all duration-1000 ${
+        visibility ? " -translate-x-[0px]" : " -translate-x-[1024px]"
+      }`}
+    >
+      <div className="w-full h-full flex flex-row">
+        <div className="self-stretch flex-1 bg-white pt-5 px-4 flex flex-col">
+          <div
+            className={`self-end transition-all duration-1000 cursor-pointer ${
+              visibility ? "opacity-100" : "opacity-0"
+            } `}
+            onClick={onCloseButtonClicked}
+          >
+            <CLoseIcon />
+          </div>
 
-                    <div>
-                        {
-                            sidebardata.map((item, index) => {
-
-                                return (
-                                    <div
-                                        key={index + ''}
-                                        className='flex items-center pl-[26px] py-[15px] space-x-[22px] cursor-pointer relative'
-                                        style={{
-                                            backgroundColor: router.pathname == item.path ? '#FAFAFC' : 'white'
-                                        }}
-                                        onClick={() => {
-                                            router.push(item.path)
-                                            onCloseButtonClicked()
-                                        }}
-                                    >
-                                        <div>
-                                            {item.Icon}
-                                        </div>
-                                        <div>
-                                            <h1 className='text-[#807C7C] text-base font-normal'>{item.title}</h1>
-                                        </div>
-                                        {router.pathname == item.path && <div className='absolute -left-[24px] top-0 bottom-0 w-1 bg-[#B3DD07]'></div>}
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+          <div>
+            {sidebardata.map((item, index) => {
+              return (
+                <div
+                  key={index + ""}
+                  className="flex items-center pl-[26px] py-[15px] space-x-[22px] cursor-pointer relative"
+                  style={{
+                    backgroundColor:
+                      router.pathname == item.path ? "#FAFAFC" : "white",
+                  }}
+                  onClick={() => {
+                    router.push(item.path);
+                    onCloseButtonClicked();
+                  }}
+                >
+                  <div>{item.Icon}</div>
+                  <div>
+                    <h1 className="text-[#807C7C] text-base font-normal">
+                      {item.title}
+                    </h1>
+                  </div>
+                  {router.pathname == item.path && (
+                    <div className="absolute -left-[24px] top-0 bottom-0 w-1 bg-[#B3DD07]"></div>
+                  )}
                 </div>
-                <div className='flex-1 self-stretch hidden sm:block '
-                    onClick={onCloseButtonClicked}></div>
-            </div>
+              );
+            })}
+          </div>
         </div>
-    );
-}
+        <div
+          className="flex-1 self-stretch hidden sm:block "
+          onClick={onCloseButtonClicked}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 const CLoseIcon = () => {
-    return (<svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-7 h-7"
-
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-7 h-7"
     >
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18 18 6M6 6l12 12"
-        />
-    </svg>)
-}
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
 
 export default MobileSidebar;

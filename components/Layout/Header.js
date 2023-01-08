1ed082f89d10670bb83ab8772f84d6b1dcbc9707
unknown <saliheenafridi0@gmail.com>
2023-01-08 22:09:50 +0500

    salih

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,61 +1,64 @@
-import { Menu } from '@headlessui/react';
-import Link from 'next/link';
-import React from 'react';
-import Notifications from '../Notifications'
-import ProfileMenu from '../ProfileMenu';
-import HeaderNotificationsIcon from '../SVGs/HeaderIcons/HeaderNotificationsIcon';
-import HeaderSearchIcon from '../SVGs/HeaderIcons/HeaderSearchIcon';
+import { Menu } from "@headlessui/react";
+import Link from "next/link";
+import React from "react";
+import Notifications from "../Notifications";
+import ProfileMenu from "../ProfileMenu";
+import HeaderNotificationsIcon from "../SVGs/HeaderIcons/HeaderNotificationsIcon";
+import HeaderSearchIcon from "../SVGs/HeaderIcons/HeaderSearchIcon";
 
-const Header = ({ onMenuButtonClicked }) =>
-{
-    return (
-        <div className='h-[80px] bg-white flex items-center justify-between pl-[32px] pr-[42px]'>
-            <div>
-                <h1 className='text-black text-lg hidden lg:inline'>Welcome To My Tennis Dairy Dashboard</h1>
-                <div className='bolck lg:hidden cursor-pointer'
-                    onClick={ () => onMenuButtonClicked() }
-                >
-                    <MenuIcon />
-                </div>
-            </div>
-            <div className='flex space-x-[25px] items-center '>
-                <div className='hidden sm:block sm:w-[312px] border border-[#D0CECE] h-[40px] rounded-md overflow-hidden relative'>
-                    <div className='h-full flex items-center justify-center absolute  left-[17px]  '>
-                        <HeaderSearchIcon />
-                    </div>
-                    <input
-                        className='z-40 placeholder:font-normal font-normal text-dark-gray placeholder:text-base 
-                                   w-full h-full pr-3 pl-[56px] outline-none border-none text-base'
-                        placeholder='Search'
-                    />
-                </div>
-                <div>
-                    <Notifications />
-                </div>
-                <div className='relative z-50'>
-                    <ProfileMenu />
-                </div>
-            </div>
-        </div >
-    );
-}
+const Header = ({ onMenuButtonClicked }) => {
+  return (
+    <div className="h-[80px] bg-white flex items-center justify-between pl-[32px] pr-[42px]">
+      <div>
+        <h1 className="text-black text-lg hidden lg:inline">
+          Welcome To My Tennis Dairy Dashboard
+        </h1>
+        <div
+          className="bolck lg:hidden cursor-pointer"
+          onClick={() => onMenuButtonClicked()}
+        >
+          <MenuIcon />
+        </div>
+      </div>
+      <div className="flex space-x-[25px] items-center ">
+        <div className="hidden sm:block sm:w-[312px] border border-[#D0CECE] h-[40px] rounded-md overflow-hidden relative">
+          <div className="h-full flex items-center justify-center absolute  left-[17px]">
+            <HeaderSearchIcon />
+          </div>
+          <input
+            className="z-40 placeholder:font-normal font-normal text-dark-gray placeholder:text-base 
+                                   w-full h-full pr-3 pl-[56px] outline-none border-none text-base"
+            placeholder="Search"
+          />
+        </div>
+        <div>
+          <Notifications />
+        </div>
+        <div className="relative z-40">
+          <ProfileMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const MenuIcon = () =>
-{
-    return (<svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={ 1.5 }
-        stroke="currentColor"
-        className="w-8 h-8"
+const MenuIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-8 h-8"
     >
-        <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-    </svg>)
-}
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+};
 
 export default Header;
---
+++ components/SVGs/LocationIcon.js
@@ -0,0 +1,12 @@
+const LocationIcon = () => {
+  return (
+    <svg width={16} height={20} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8 0a8.009 8.009 0 0 0-8 8c0 3.255 2.363 5.958 4.866 8.82.792.905 1.612 1.843 2.342 2.79a1 1 0 0 0 1.584 0c.73-.947 1.55-1.884 2.342-2.79C13.637 13.957 16 11.254 16 8a8.01 8.01 0 0 0-8-8Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"
+        fill="#B3DD07"
+      />
+    </svg>
+  );
+};
+
+export default LocationIcon;
